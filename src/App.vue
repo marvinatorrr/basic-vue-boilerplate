3e4faa51adff72c8f409b172cb8c168db92e0138
Marvin <marvinatorrr@gmail.com>
2023-07-30 01:00:10 +0800

    initn

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,50 @@
 <template>
   <div id="app">
     <div class="nav">
-      <router-link to="/">Home</router-link>|<router-link to="/about"
-        >About</router-link
-      >
+      <b-button @click="openNewWindow">Click me for a pop up!</b-button>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
+<script>
+export default {
+  methods: {
+    async openNewWindow() {
+      console.log("button is clicked!");
+      const newWindow = window.open("", "_blank", "width=400,height=300");
 
-:root {
-  --font: Roboto, sans-serif;
-  --textColor: #374961;
-  --linkActiveColor: #41b783;
-}
+      const bootstrapCSS = newWindow.document.createElement("link");
+      bootstrapCSS.rel = "stylesheet";
+      bootstrapCSS.href =
+        "https://cdn.jsdelivr.net/npm/bootstrap@4.0/dist/css/bootstrap.min.css";
+      newWindow.document.head.appendChild(bootstrapCSS);
 
-#app {
-  font-family: var(--font);
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: var(--textColor);
+      const bootstrapVueCSS = newWindow.document.createElement("link");
+      bootstrapVueCSS.rel = "stylesheet";
+      bootstrapVueCSS.href =
+        "https://cdn.jsdelivr.net/npm/bootstrap-vue@2.0/dist/bootstrap-vue.css";
+      newWindow.document.head.appendChild(bootstrapVueCSS);
 
-  .logo {
-    width: 20%;
-  }
-}
+      const appContainer = newWindow.document.createElement("div");
+      newWindow.document.body.appendChild(appContainer);
 
-.nav {
-  padding: 30px 0 100px 0;
+      const [
+        { default: Vue },
+        { default: Popup },
+        { default: BootstrapVue },
+      ] = await Promise.all([
+        import("vue"),
+        import("./components/Popup.vue"),
+        import("bootstrap-vue"),
+      ]);
 
-  a {
-    font-weight: 500;
-    color: var(--textColor);
-    margin: 0 5px;
-  }
+      Vue.use(BootstrapVue);
 
-  a.router-link-exact-active {
-    color: var(--linkActiveColor);
-  }
-}
-</style>
+      new Vue({
+        el: appContainer,
+        render: (h) => h(Popup),
+      });
+    },
+  },
+};
+</script>
---
+++ src/components/Popup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    hi this is my pop up
+    <b-button @click="onClick">click me!</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      console.log("hello world!");
+    },
+  },
+};
+</script>
+
+<style></style>
